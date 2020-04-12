55985dded1a4cf3f7b14fe883f214ba5d971307d
brfbrf <advev@mail.ru>
2020-04-12 03:33:18 +0300

    Initial Commit

---
+++ Event-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Event-iOS
+//
+//  Created by M1 on 12.04.2020.
+//  Copyright © 2020 Dmitry Alexandrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
